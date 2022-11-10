c07e86afc828451f6f61f77413815ca355e35f82
GregoireALP <gregoire.alperovitch@gmail.com>
2022-11-10 19:16:11 +0100

    Fix Tableaux + Ranked blocs

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-grid = []
+import blocs
+
+cercle_liste = [i for i in range(0, len(blocs.blocs_liste_commun) + len(blocs.blocs_liste_cercle))]
+losange_liste = [i for i in range(0, 20)] +  [y for y in range(len(cercle_liste), len(cercle_liste) + 14)]
+triangle_liste = [i for i in range(0, 20)] + [y for y in range(20 + len(blocs.blocs_liste_cercle) + len(blocs.blocs_liste_losange) ,len(blocs.blocs_liste_commun) + len(blocs.blocs_liste_cercle) + len(blocs.blocs_liste_losange) + len(blocs.blocs_liste_triangle))]
+
+def afficher(bloc):
+    for i in range(len(bloc)):
+        for y in range(len(bloc[i])):
+            if bloc[i][y] == 2:
+                print("*", end=" ")
+            else:
+                print(" ", end=" ")
+        print()
+
+
+afficher(blocs.blocs_liste[losange_liste[len(losange_liste) - 1]])
